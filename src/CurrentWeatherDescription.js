56112060bea1f786097669f56c7b9c576376313a
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-05-14 15:52:09 +0100

    Added icon to current weather description

--- src/CurrentWeatherDescription.js
+++ src/CurrentWeatherDescription.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./CurrentWeatherDescription.css";
 import { FaWind } from "react-icons/fa";
 import { WiHumidity } from "react-icons/wi";
+import { FaThermometerHalf } from "react-icons/fa";
 
 export default function CurrentWeatherDescription() {
     return (
       <ul className="current-weather-description">
-        <li><WiHumidity /></li>
-        <li><FaWind /></li>
-        <li>Something else</li>
+        <li><WiHumidity /> Humidity: </li>
+        <li><FaWind /> Wind speed:</li>
+        <li><FaThermometerHalf /> Feels like:</li>
       </ul>
     );
 }
