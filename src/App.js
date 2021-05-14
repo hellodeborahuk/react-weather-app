8a45c7106a1d9dbc06fe2539cfbefb2651ae90c0
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-05-14 11:05:56 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        <a
-          href="https://github.com/hellodeborahuk/react-weather-app"
-          target="_blank"
-        >
-          Open-source code
-        </a>{" "}
-        by Debbie Dann
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          <a
+            href="https://github.com/hellodeborahuk/react-weather-app"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Debbie Dann
+        </footer>
+      </div>
     </div>
   );
 }
