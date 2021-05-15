8a7b348e6fe44ca0c7c55d674e69432f542f941e
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-05-15 21:09:42 +0100

    Removed unused functions from unit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -3,16 +3,6 @@ import React, { useState } from "react";
 export default function WeatherTemperature(props) {
 const [unit, setUnit] = useState("celsius");
 
-function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-}
-
-function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-}
-
 function fahrenheitTemp() {
     return Math.round((props.celsius * 9) / 5 + 32);
 }
