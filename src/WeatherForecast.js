ed3a999f73b58c194de43da55776dfe9cff0fc9d
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-05-27 16:09:07 +0100

    added else to correct error

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -29,6 +29,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
