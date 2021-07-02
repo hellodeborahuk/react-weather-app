2ed3d5266d1bff73f562cab93abd4b07f385fc1f
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-07-02 21:11:49 +0100

    Removed C to F toggle functionality

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,46 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-const [unit, setUnit] = useState("celsius");
-
-function fahrenheitTemp() {
-    return Math.round((props.celsius * 9) / 5 + 32);
-}
-
-function toggleUnit(event) {
-    event.preventDefault();
- if (unit === "celsius") {
-     setUnit("fahrenheit");
- } else {
-     setUnit("celsius");
- }
-}
-
-let unitSelect;
-if (unit === "celsius") {
-    unitSelect = (
-      <span className="temperature-unit">
-        °C |{" "}
-        <a href="/" onClick={toggleUnit}>
-          °F
-        </a>
-      </span>
-    );
-} else {
-    unitSelect = (
-      <span className="temperature-unit">
-        <a href="/" onClick={toggleUnit}>
-          °C
-        </a>{" "}
-        | °F
-      </span>
-    );
-}
-
 return (
   <div className="WeatherTemperature">
-    <span className="temperature-number">{unit === "celsius" ? props.celsius : fahrenheitTemp()}</span>
-   {unitSelect}
+    <span className="temperature-number">
+      {props.celsius}
+      <span className="temperature-unit">°C</span>
+    </span>
   </div>
 );
     
